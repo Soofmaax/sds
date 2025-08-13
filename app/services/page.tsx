30d7035c5831f66841102cf9350dcafbf011a378
Soofmax <124178747+Soofmaax@users.noreply.github.com>
2025-08-14 01:46:37 +0200

    Create page.tsx

---
+++ app/services/page.tsx
@@ -0,0 +1,7 @@
+export default function ServicesPage() {
+  return (
+    <main className="container mx-auto py-24">
+      <h1 className="text-4xl font-bold">Nos Services</h1>
+    </main>
+  );
+}
